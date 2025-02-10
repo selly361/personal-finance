d73b8b8b7245aeccf93c487fa6f3ee1fbb1524fd
selly361 <seikou615@gmail.com>
2025-02-10 19:01:05 +0000

    Update: rename actions

--- actions/index.ts
+++ actions/index.ts
@@ -1,7 +1,7 @@
 /* Auth */
 
-export { signInAction } from './auth/signIn'
-export { signUpAction } from './auth/signUp'
-export { signOutAction } from './auth/signOut'
+export { loginAction } from './auth/login'
+export { registerAction } from './auth/register'
+export { logoutAction } from './auth/logout'
 export { forgotPasswordAction } from './auth/forgotPassword'
 export { resetPasswordAction } from './auth/resetPassword'
