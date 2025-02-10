44b25e1f8992a5e20cee77e9457babe4202d2607
selly361 <seikou615@gmail.com>
2025-02-10 19:04:34 +0000

    Refactor: Add responsive design, branding, and authentication UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { LargeLogo } from '@/components/icons'
 import { Metadata } from 'next/types'
 import { PropsWithChildren } from 'react'
 
@@ -8,9 +9,24 @@ export const metadata: Metadata = {
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
-    <div className='max-w-7xl flex flex-col gap-12 items-start'>
-      <div>Side image</div>
-      {children}
-    </div>
+    <main className='w-screen h-screen p-5 flex max-lg:h-max max-lg:p-0 max-lg:flex-col max-lg:justify-between max-lg:pb-10 max-lg:gap-10 max-lg:items-center'>
+      <header className='hidden max-lg:flex w-screen min-h-20 bg-grey-900 justify-center items-center'>
+        <LargeLogo />
+      </header>
+      <div className="bg-[url('/illustration-authentication.svg')] bg-cover bg-no-repeat bg-center max-h-screen rounded-md pt-5 pl-20 pb-10 pr-5 flex flex-col justify-between max-w-[35%] max-lg:hidden">
+        <LargeLogo />
+        <div className='text-white'>
+          <h2>Keep track of your money and save for your future</h2>
+          <p className='mt-6'>
+            Personal finance app puts you in control of your spending. Track
+            transactions, set budgets, and add to savings pots easily.
+          </p>
+        </div>
+      </div>
+
+      <div className='max-lg:w-[70vw] max-sm:w-[90vw] lg:w-full lg:flex lg:justify-center lg:items-center'>
+        <div className='lg:w-[35rem] bg-white rounded-xl p-8'>{children}</div>
+      </div>
+    </main>
   )
 }
