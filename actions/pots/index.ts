30e9a76c26f1b2c0686a7188784264122c5a64b8
selly361 <seikou615@gmail.com>
2025-02-16 18:43:54 +0000

    Add server CRUD actions for `pots`

---
+++ actions/pots/index.ts
@@ -0,0 +1,65 @@
+'use server'
+
+import { Pot } from '@/types'
+import { createServer } from '@/lib/supabase'
+import { potValidation } from '@/lib/validations'
+import { revalidatePath } from 'next/cache'
+
+export const getPots = async () => {
+  const supabase = await createServer()
+
+  const { data, error } = await supabase.from('budgets').select('*')
+
+  if (error) throw error
+
+  return data
+}
+
+export const createPot = async (potData: Pot) => {
+  const result = potValidation.safeParse(potData)
+
+  if (!result.success) return { error: result.error.format() }
+
+  const supabase = await createServer()
+
+  const { data, error } = await supabase.from('pots').insert([potData]).select().single()
+
+  if (error) return { error: error.message }
+
+  revalidatePath('/')
+  revalidatePath('/pots')
+  return { data }
+}
+
+export const updatePot = async (pot_id: string, potData: Pot) => {
+  const result = potValidation.safeParse(potData)
+
+  if (!result.success) return { error: result.error.format() }
+
+  const supabase = await createServer()
+
+  const { data, error } = await supabase
+    .from('pots')
+    .update(potData)
+    .eq('id', pot_id)
+    .select()
+    .single()
+
+  if (error) return { error: error.message }
+
+  revalidatePath('/')
+  revalidatePath('/pots')
+  return { data }
+}
+
+export const deletePot = async (pot_id: string) => {
+  const supabase = await createServer()
+
+  const { error } = await supabase.from('pots').delete().eq('id', pot_id)
+
+  if (error) return { error: error.message }
+
+  revalidatePath('/')
+  revalidatePath('/pots')
+  return { success: true }
+}
