ebdcce84eedf3e5e03fbddb1d3a2405402d186da
selly361 <seikou615@gmail.com>
2025-02-16 18:41:25 +0000

    Add types

---
+++ types/index.ts
@@ -0,0 +1,30 @@
+import { budgetValidation, potValidation, transactionValidation } from '@/lib/validations'
+
+import { z } from 'zod'
+
+/* ------------------- Budget Type ------------------- */
+
+export type Budget = z.infer<typeof budgetValidation>
+
+/* ------------------- Pot Type ------------------- */
+
+export type Pot = z.infer<typeof potValidation>
+
+/* ------------------- Transaction Type ------------------- */
+
+export type Transaction = z.infer<typeof transactionValidation>
+
+/* ------------------- Theme Type ------------------- */
+
+export type Theme = {
+  id: string
+  name: string
+  color_code: string
+}
+
+/* ------------------- Category Type ------------------- */
+
+export type Category = {
+  id: string
+  name: string
+}
