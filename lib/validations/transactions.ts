2b6047137874513bb534ffa95a7b47f3426b856c
selly361 <seikou615@gmail.com>
2025-02-16 18:35:50 +0000

    Add `transactions` zod validation

---
+++ lib/validations/transactions.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+
+export const transactionValidation = z.object({
+  recipient_sender_name: z
+    .string()
+    .trim()
+    .min(1, 'Name is required')
+    .max(20, 'Name must be less than 20 characters')
+    .regex(
+      /^[\w\s\-]+$/,
+      'Name must only contain alphanumeric characters, spaces, and hyphens'
+    ),
+  category_id: z.string().uuid(),
+  avatar: z
+    .string()
+    .trim()
+    .min(1, 'An avatar is required')
+    .url('Avatar must be a valid URL'),
+  date: z
+    .date()
+    .refine((date) => !isNaN(date.getTime()), 'Date must be a valid date')
+    .refine((date) => date <= new Date(), 'Date cannot be in the future'),
+  amount: z
+    .number()
+    .min(-1_000_000, 'Amount must be greater than -1,000,000')
+    .max(1_000_000, 'Amount must be less than 1,000,000')
+    .refine((value) => Number.isInteger(value) || Number(value) === value, {
+      message: 'Amount must be a valid integer or a valid decimal',
+    }),
+  recurring: z.boolean(),
+})
