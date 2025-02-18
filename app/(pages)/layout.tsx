367278b5c9ef1930ecc48eb10e82c495c5e579f5
MoreForBegs <seikou615@gmail.com>
2025-02-18 15:30:37 +0000

    Refactor layout styles

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,9 +3,9 @@ import { Sidebar } from '@/components'
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
-    <div className='w-screen flex gap-12 items-start'>
+    <div className='w-screen flex items-start'>
       <Sidebar />
-      {children}
+      <main className='px-10 pt-8 w-full h-screen'>{children}</main>
     </div>
   )
 }
