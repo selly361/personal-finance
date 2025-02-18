327049989ae0236970eb66bbbf5a044e0fd007e3
selly361 <seikou615@gmail.com>
2025-02-18 09:08:48 +0000

    Change `getThemes` into `getThemesWithBudgetUsage` and `getThemesWithPotUsage`

--- actions/getThemes.ts
+++ actions/getThemes.ts
@@ -2,12 +2,20 @@
 
 import { createServer } from '@/lib/supabase'
 
-export const getThemes = async () => {
+export const getThemesWithBudgetUsage = async () => {
   const supabase = await createServer()
+  const { data, error } = await supabase.rpc('get_themes_with_budget_usage')
+  
+  if (error) throw error
+  
+  return data.map((theme) => ({ ...theme, isUsed: theme.isused }))
+}
 
-  const { data, error } = await supabase.from('themes').select('*')
+export const getThemesWithPotUsage = async () => {
+  const supabase = await createServer()
+  const { data, error } = await supabase.rpc('get_themes_with_pot_usage')
 
   if (error) throw error
 
-  return data
+  return data.map((theme) => ({ ...theme, isUsed: theme.isused }))
 }
