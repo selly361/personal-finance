fa3f96092ee075becff2bb5b626e363fb720744d
MoreForBegs <seikou615@gmail.com>
2025-02-18 12:01:25 +0000

    Use type `Database` for the `createServerClient`

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,22 +1,23 @@
-import { cookies } from 'next/headers'
-import { createServerClient } from '@supabase/ssr'
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+import { Database } from "./database.types";
 
 export const createServer = async () => {
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll()
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options)
-            })
+              cookieStore.set(name, value, options);
+            });
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -25,5 +26,5 @@ export const createServer = async () => {
         },
       },
     }
-  )
-}
+  );
+};
