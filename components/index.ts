b23ee565eeab8b462a0fbfbe7a737fd291d74190
MoreForBegs <seikou615@gmail.com>
2025-02-18 15:33:32 +0000

    Export `CategorySelect` and `ModalDialog`

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export { default as SubmitButton } from './SubmitButton'
 export { default as Sidebar } from './Sidebar'
 export { default as ThemeSelect } from './ThemeSelect'
+export { default as CategorySelect } from './CategorySelect'
+export { default as ModalDialog } from './ModalDialog'
