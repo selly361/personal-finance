5702064637dcbab3e90b29c8424edcaf03dba5ed
MoreForBegs <seikou615@gmail.com>
2025-02-18 15:34:17 +0000

    Add `NewBudgetModal`

---
+++ app/(pages)/budgets/_components/NewBudgetModal.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useState } from 'react'
+import { ModalDialog } from '@/components'
+import NewBudgetForm from './NewBudgetForm'
+
+function NewBudgetModal() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <ModalDialog
+      title='Add New Budget'
+      description='Choose a category to set a spending budget. These categories can help you monitor spending.'
+      triggerLabel='+ Add New Budget'
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      onOpen={() => setIsOpen(true)}
+    >
+      <NewBudgetForm onSuccess={() => setIsOpen(false)} />
+    </ModalDialog>
+  )
+}
+
+export default NewBudgetModal
