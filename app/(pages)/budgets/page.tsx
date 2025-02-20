66cad01ee37f0ca070ebe2a59207995322c2132b
MoreForBegs <seikou615@gmail.com>
2025-02-20 18:15:00 +0000

    "Enhance 'Budgets' page with dynamic data and components for budget tracking

--- app/(pages)/budgets/page.tsx
+++ app/(pages)/budgets/page.tsx
@@ -1,4 +1,10 @@
+import { getBudgetsWithDetails } from '@/actions'
+
+import BudgetPieChart from './_components/BudgetPieChart'
+import { Fragment } from 'react'
 import { Metadata } from 'next/types'
+import NewBudgetModal from './_components/NewBudgetModal'
+import SpendingSummary from './_components/SpendingSummary'
 
 export const metadata = {
   title: 'Budgets',
@@ -6,10 +12,21 @@ export const metadata = {
 } as Metadata
 
 async function Page() {
+  const budgets = await getBudgetsWithDetails()
+
   return (
-    <main>
-      <h1>Budgets</h1>
-    </main>
+    <Fragment>
+      <section className='w-full flex justify-between items-center'>
+        <h1>Budgets</h1>
+        <NewBudgetModal />
+      </section>
+      <section className='w-full'>
+        <div className='w-1/3 bg-white p-8 rounded-xl'>
+          <BudgetPieChart className='mx-auto' budgets={budgets} />
+          <SpendingSummary budgets={budgets} />
+        </div>
+      </section>
+    </Fragment>
   )
 }
 
