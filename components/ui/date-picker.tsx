e21a5f4df29f089e9734262e73c65bdd137a445e
selly361 <seikou615@gmail.com>
2025-02-23 14:44:18 +0000

    Add `react-date-picker` and `checkbox` shadcn ui components

---
+++ components/ui/date-picker.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { useEffect, useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { format } from 'date-fns'
+
+function DatePicker({
+  selected,
+  onSelect,
+}: {
+  selected?: Date
+  onSelect: (date: Date | undefined) => void
+}) {
+  const [defaultDate, setDefaultDate] = useState<Date>(new Date())
+
+  useEffect(() => {
+    if (!selected) {
+      onSelect(defaultDate)
+    }
+  }, [selected, onSelect, defaultDate])
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-full justify-start text-left font-normal',
+            !selected && 'text-muted-foreground'
+          )}
+        >
+          <CalendarIcon className='mr-2 h-4 w-4' />
+          {selected ? format(selected, 'PPP') : format(defaultDate, 'PPP')}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-auto p-0'>
+        <Calendar
+          mode='single'
+          selected={selected || defaultDate}
+          onSelect={onSelect}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export { DatePicker }
