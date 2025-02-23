1220647491bacbed27ddac5b46a44df452611744
MoreForBegs <seikou615@gmail.com>
2025-02-23 21:48:47 +0000

    Update: add Budgets component and improve layout with flexible section spacing

--- app/(pages)/budgets/page.tsx
+++ app/(pages)/budgets/page.tsx
@@ -5,6 +5,7 @@ import { Fragment } from 'react'
 import { Metadata } from 'next/types'
 import NewBudgetModal from './_components/NewBudgetModal'
 import SpendingSummary from './_components/SpendingSummary'
+import Budgets from './_components/Budgets'
 
 export const metadata = {
   title: 'Budgets',
@@ -20,11 +21,12 @@ async function Page() {
         <h1>Budgets</h1>
         <NewBudgetModal />
       </section>
-      <section className='w-full'>
-        <div className='w-1/3 bg-white p-8 rounded-xl'>
+      <section className='w-full flex gap-8'>
+        <div className='w-1/3 bg-white p-8 rounded-xl h-max'>
           <BudgetPieChart className='mx-auto' budgets={budgets} />
           <SpendingSummary budgets={budgets} />
         </div>
+        <Budgets />
       </section>
     </Fragment>
   )
