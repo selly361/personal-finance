ac9415bf058c9b1d0fdae119c7faf7266bc56741
selly361 <seikou615@gmail.com>
2025-02-23 14:44:48 +0000

    Refactor getthemes action

--- actions/getThemes.ts
+++ actions/getThemes.ts
@@ -1,21 +1,32 @@
 'use server'
 
+import { Theme } from '@/types'
 import { createServer } from '@/lib/supabase'
 
-export const getThemesWithBudgetUsage = async () => {
+export const getThemesWithBudgetUsage = async (): Promise<Theme[]> => {
   const supabase = await createServer()
   const { data, error } = await supabase.rpc('get_themes_with_budget_usage')
-  
+
   if (error) throw error
-  
-  return data.map((theme) => ({ ...theme, isUsed: theme.isused }))
+
+  return data.map((theme) => ({
+    id: theme.id,
+    name: theme.name,
+    color: theme.color_code,
+    isUsed: theme.isused,
+  }))
 }
 
-export const getThemesWithPotUsage = async () => {
+export const getThemesWithPotUsage = async (): Promise<Theme[]> => {
   const supabase = await createServer()
   const { data, error } = await supabase.rpc('get_themes_with_pot_usage')
 
   if (error) throw error
 
-  return data.map((theme) => ({ ...theme, isUsed: theme.isused }))
+  return data.map((theme) => ({
+    id: theme.id,
+    name: theme.name,
+    color: theme.color_code,
+    isUsed: theme.isused,
+  }))
 }
