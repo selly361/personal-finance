4298bd54ba7d262c10cdef0fd1725ae933c6ba0f
selly361 <seikou615@gmail.com>
2025-02-23 14:50:05 +0000

    Remove `p-6` style

--- app/(pages)/budgets/_components/SpendingSummary/index.tsx
+++ app/(pages)/budgets/_components/SpendingSummary/index.tsx
@@ -1,6 +1,6 @@
-import { cn } from '@/lib/utils'
-import BudgetItem from './BudgetItem'
 import { BudgetDetails } from '@/types'
+import BudgetItem from './BudgetItem'
+import { cn } from '@/lib/utils'
 
 interface SpendingSummaryProps {
   budgets: BudgetDetails[]
@@ -8,7 +8,7 @@ interface SpendingSummaryProps {
 
 export default function SpendingSummary({ budgets }: SpendingSummaryProps) {
   return (
-    <div className='bg-white p-6 rounded-xl'>
+    <div className='bg-white rounded-xl'>
       <h2 className='text-lg mb-4'>Spending Summary</h2>
       <div className='divide-y'>
         {budgets.map(({ id, category, total_spent, max_spend, theme }) => (
