eb1db9f6a619404a306e4376e4e0cdf8d04bfc13
selly361 <seikou615@gmail.com>
2025-03-23 14:24:19 +0000

    Use `RecurringBillsPage` component

--- app/(pages)/recurring-bills/page.tsx
+++ app/(pages)/recurring-bills/page.tsx
@@ -1,16 +1,13 @@
 import { Metadata } from 'next/types'
+import RecurringBillsPage from './_components/RecurringBillsPage'
+import { getRecurringBillsSummary } from '@/actions'
 
 export const metadata = {
   title: 'Recurring Bills',
   description: 'Track and manage your recurring expenses easily.',
 } as Metadata
 
-async function Page() {
-  return (
-    <main>
-      <h1>Recurring Bills</h1>
-    </main>
-  )
+export default async function Page() {
+  const recurringBillsData = await getRecurringBillsSummary()
+  return <RecurringBillsPage data={recurringBillsData} />
 }
-
-export default Page
