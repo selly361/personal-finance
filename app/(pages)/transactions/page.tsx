83baa3ebc1943ea83a951749cbd72751011db22b
selly361 <seikou615@gmail.com>
2025-03-25 16:32:00 +0000

    Use `TransactionsPage` component

--- app/(pages)/transactions/page.tsx
+++ app/(pages)/transactions/page.tsx
@@ -1,21 +1,28 @@
-import { Fragment } from 'react'
-import { Metadata } from 'next/types'
-import NewTransactionModal from './_components/NewTransactionModal'
+import { Fragment } from "react"
+import { Metadata } from "next/types"
+import NewTransactionModal from "./_components/NewTransactionModal/NewTransactionModal"
+import TransactionsPage from "./_components/TransactionsPage"
+import { getTransactionsOverview } from "@/actions/transactions"
 
 export const metadata = {
-  title: 'Transactions',
-  description: 'View and track all your spending in one place.',
+  title: "Transactions",
+  description: "View and track all your spending in one place.",
 } as Metadata
 
-async function Page() {
+export default async function Page() {
+  const overview = await getTransactionsOverview()
+
   return (
     <Fragment>
-      <section className='w-full flex justify-between items-center'>
-        <h1>Transactions</h1>
+      <section className="w-full flex justify-between items-center mb-4">
+        <h1 className="text-xl font-bold">Transactions</h1>
         <NewTransactionModal />
       </section>
+
+      <TransactionsPage
+        transactions={overview.transactions}
+        categories={overview.categories}
+      />
     </Fragment>
   )
 }
-
-export default Page
