5db8deb8c133af20f1f74d2efc9c405e2dc237c0
selly361 <seikou615@gmail.com>
2025-03-30 19:25:04 +0100

    Feat: add current balance

--- app/(pages)/(overview)/_components/SummaryStats.tsx
+++ app/(pages)/(overview)/_components/SummaryStats.tsx
@@ -5,7 +5,11 @@ function SummaryStats({ income, expenses }: Summary) {
   return (
     <section className='w-full flex gap-2'>
       <div className='h-32 w-full p-6 bg-grey-900 rounded-xl text-white flex flex-col gap-4'>
-        <h6>Income</h6>
+        <h6>Current Balance</h6>
+        <p className='text-xl font-bold'>£{(income - expenses).toLocaleString('en-UK')}</p>
+      </div>
+      <div className='h-32 w-full p-6 bg-white rounded-xl flex flex-col gap-4'>
+        <h6 className='text-grey-500'>Income</h6>
         <p className='text-xl font-bold'>£{income.toLocaleString('en-UK')}</p>
       </div>
       <div className='h-32 w-full p-6 bg-white rounded-xl flex flex-col gap-4'>
