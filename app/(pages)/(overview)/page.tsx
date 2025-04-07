fe9bd7297661ef26dc7d1147bb7014c55cfbfaa2
selly361 <seikou615@gmail.com>
2025-04-07 14:14:38 +0100

    feat(overview): add new layout with budgets, pots, recurring bills, and transactions

--- app/(pages)/(overview)/page.tsx
+++ app/(pages)/(overview)/page.tsx
@@ -1,22 +1,48 @@
+import {
+  getBudgetsWithDetails,
+  getPotsWithDetails,
+  getRecurringBillsSummary,
+  getTransactionsOverview,
+  logoutAction,
+} from '@/actions'
+
+import BudgetsOverviewCard from './_components/BudgetsOverviewCard'
 import { Button } from '@/components/ui/button'
 import { Fragment } from 'react'
 import { LogOutIcon } from 'lucide-react'
 import { Metadata } from 'next/types'
+import PotsOverviewCard from './_components/PotsOverviewCard'
+import RecurringBillsOverviewCard from './_components/RecurringBillsOverviewCard'
 import SummaryStats from './_components/SummaryStats'
+import TransactionsOverviewCard from './_components/TransactionsOverviewCard'
 import { getSummary } from '@/actions/getSummary'
-import { logoutAction } from '@/actions'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Overview',
-  description:
-    'Get an overview of your finances, track spending, and manage budgets all in one place.',
-} as Metadata
+  description: 'Get an overview of your finances...',
+}
 
 async function Page() {
-  const summary = await getSummary()
+  const [
+    summary,
+    pots,
+    recurringData,
+    budgets,
+    transactionsOverview,
+  ] = await Promise.all([
+    getSummary(),
+    getPotsWithDetails(),
+    getRecurringBillsSummary(),
+    getBudgetsWithDetails(),
+    getTransactionsOverview(),
+  ])
+
+  const latestTransactions = transactionsOverview.transactions.slice(0, 5)
+  const recurringBillsSummary = recurringData.summary
+
   return (
     <Fragment>
-      <section className='w-full flex justify-between items-center'>
+      <section className="w-full flex justify-between items-center">
         <h1>Overview</h1>
         <Button formAction={logoutAction}>
           <LogOutIcon />
@@ -24,6 +50,16 @@ async function Page() {
         </Button>
       </section>
       <SummaryStats {...summary} />
+      <section className="flex gap-6">
+        <section className="w-full flex flex-col gap-6">
+          <PotsOverviewCard pots={pots} />
+          <TransactionsOverviewCard transactions={latestTransactions} />
+        </section>
+        <section className="w-full flex flex-col gap-6">
+          <BudgetsOverviewCard budgets={budgets} />
+          <RecurringBillsOverviewCard summary={recurringBillsSummary} />
+        </section>
+      </section>
     </Fragment>
   )
 }
