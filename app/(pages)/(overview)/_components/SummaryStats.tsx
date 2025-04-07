fc53e0c0ef01560a1c8bdab1066b6899e9c1f9cc
selly361 <seikou615@gmail.com>
2025-04-07 14:16:59 +0100

    feat: use `millify` for currency formatting in `SummaryStats`

--- app/(pages)/(overview)/_components/SummaryStats.tsx
+++ app/(pages)/(overview)/_components/SummaryStats.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import { Summary } from '@/types'
+'use client'
 
-function SummaryStats({ income, expenses }: Summary) {
+import { Summary } from '@/types'
+import millify from 'millify'
+
+export default function SummaryStats({ income, expenses }: Summary) {
+  const balance = income - expenses
   return (
     <section className='w-full flex gap-2'>
       <div className='h-32 w-full p-6 bg-grey-900 rounded-xl text-white flex flex-col gap-4'>
         <h6>Current Balance</h6>
-        <p className='text-xl font-bold'>£{(income - expenses).toLocaleString('en-UK')}</p>
+        <p className='text-xl font-bold'>£{millify(balance, { precision: 3 })}</p>
       </div>
       <div className='h-32 w-full p-6 bg-white rounded-xl flex flex-col gap-4'>
         <h6 className='text-grey-500'>Income</h6>
-        <p className='text-xl font-bold'>£{income.toLocaleString('en-UK')}</p>
+        <p className='text-xl font-bold'>£{millify(income, { precision: 3 })}</p>
       </div>
       <div className='h-32 w-full p-6 bg-white rounded-xl flex flex-col gap-4'>
         <h6 className='text-grey-500'>Expenses</h6>
-        <p className='text-xl font-bold'>£{expenses.toLocaleString('en-UK')}</p>
+        <p className='text-xl font-bold'>£{millify(expenses, { precision: 3 })}</p>
       </div>
     </section>
   )
 }
-
-export default SummaryStats
